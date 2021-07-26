5701fa1af1394a653d49bc31bc788a326695bd6a
fatima784 <69567648+fatima784@users.noreply.github.com>
2021-07-26 15:26:19 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from "./components/hello";
+
+function App() {
+  return <Hello/>
+    
+  }
+export default App;
---
+++ src/components/hello.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Hello(){
+    return(
+        <h1>hello world</h1>
+    )
+}
+export default Hello;
